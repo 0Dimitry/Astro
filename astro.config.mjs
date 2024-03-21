46b3cbb7a86d7e1b5b7db977449748bdd2a3cca4
0Dimitry <santiagocaballero279@gmail.com>
2024-03-20 21:57:16 -0500

    add preact

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,9 @@
 import { defineConfig } from 'astro/config';
 
+import preact from "@astrojs/preact";
+
 // https://astro.build/config
 export default defineConfig({
-    site: "https://tutorialastrp.netlify.app"
+  site: "https://tutorialastrp.netlify.app",
+  integrations: [preact()]
 });
